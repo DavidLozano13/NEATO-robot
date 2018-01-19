427cbce74ec5ee93287f12ff4ec9172797e3017a
DavidLozano13 <davidlozano1311@gmail.com>
2018-01-19 12:33:28 +0100

    Wall following

---
+++ wall_following.py
@@ -0,0 +1,164 @@
+# To import the function "envia" from the file "test_NeatoCommands.py"
+from test_NeatoCommands import envia
+import sys
+import serial
+import time
+from multiprocessing import Process, Queue
+import math
+import random
+
+###################### !!! W A R N I N G !!! ########################
+# Each group working in the same robot has to chose a different port.
+port_web_server = int(sys.argv[1])
+#####################################################################
+
+
+def set(message, timeInMs):
+    #print "SET - Message: %s, SleepTimeInMs: %f" %(message, timeInMs);
+    ser.write(message+'\r'+'\n');
+    time.sleep(abs(timeInMs/1000));
+
+def get(message):
+    #print "GET - Message: %s" %message;
+    ser.write(message+'\r'+'\n');
+
+def getLDS():
+    set('SetLDSRotation On', 1500);
+    get('GetLDSScan');
+
+    LDS_data = [];
+    while ser.inWaiting()>0:
+        line = ser.readline();
+        line_split = line.split(",");
+        if (len(line_split) == 4 and line_split[0].isdigit()):
+            #Array ['AngleInDegrees', 'DistInMM', 'errorCode']
+            line_content = [line_split[0], line_split[1], line_split[3]];
+            LDS_data.append(line_content);
+
+    set('SetLDSRotation Off', 100);
+
+    return LDS_data;
+
+def getLaserValues(sensor):
+	global speed
+	global difDestino
+	res = [2000,2000,2000,2000,2000,2000,2000,2000,2000,2000]
+	msg = getLDS()
+	values = [2000 for num in range(360)]
+	for value in msg:
+		if int(value[1]) > 0:
+			values[int(value[0])] = int(value[1])
+	# Coger 5 posiciones: 0-36, 37-72, 73-108, 109-144, 145-180
+	# Init
+	res[0] = values[0]
+	res[1] = values[36]
+	res[2] = values[72]
+	res[3] = values[108]
+	res[4] = values[144]
+	res[5] = values[180]
+	res[6] = values[216]
+	res[7] = values[252]
+	res[8] = values[288]
+	res[9] = values[324]
+
+	for i in range(0,359):
+		if i >= 0 and i <= 35:
+			# Zona izquierda
+			if values[i] < res[0] and values[i] != 0:
+				res[0] = values[i]
+		if i >= 36 and i <= 71:
+			# Zona izquierda central
+			if values[i] < res[1] and values[i] != 0:
+				res[1] = values[i]
+		if i >= 72 and i <= 107:
+			# Zona central
+			if values[i] < res[2] and values[i] != 0:
+				res[2] = values[i]
+		if i >= 108 and i <= 143:
+			# Zona derecha central
+			if values[i] < res[3] and values[i] != 0:
+				res[3] = values[i]
+		if i >= 144 and i <= 179:
+			# Zona derecha
+			if values[i] < res[4] and values[i] != 0:
+				res[4] = values[i]
+		if i >= 180 and i <= 215:
+			# Zona izquierda
+			if values[i] < res[5] and values[i] != 0:
+				res[5] = values[i]
+		if i >= 216 and i <= 251:
+			# Zona izquierda central
+			if values[i] < res[6] and values[i] != 0:
+				res[6] = values[i]
+		if i >= 252 and i <= 287:
+			# Zona central
+			if values[i] < res[7] and values[i] != 0:
+				res[7] = values[i]
+		if i >= 288 and i <= 323:
+			# Zona derecha central
+			if values[i] < res[8] and values[i] != 0:
+				res[8] = values[i]
+		if i >= 324 and i <= 359:
+			# Zona derecha
+			if values[i] < res[9] and values[i] != 0:
+				res[9] = values[i]
+
+
+
+	resfinal = [res[2], res[1], res[0], res[9], res[8]]
+	print(resfinal)
+
+	res[0]=res[0]/10
+	res[1]=res[1]/10
+	res[2]=res[2]/10
+	res[8]=res[8]/10
+	res[9]=res[9]/10
+
+	print(res)
+	print res[sensor]
+	return res[sensor]
+
+	#leftMotor.setVelocity(initialVelocity - (centralRightSensorValue + outerRightSensorValue) / 2)
+    #rightMotor.setVelocity(initialVelocity - (centralLeftSensorValue + outerLeftSensorValue) / 2 - centralSensorValue)
+
+
+if __name__ == "__main__":
+
+	# Open the Serial Port.
+	global ser
+	ser = serial.Serial(port='/dev/ttyACM0', baudrate=115200, timeout=0.05)
+	envia(ser, 'TestMode On')
+
+	envia(ser, 'PlaySound 1')
+
+	envia(ser ,'SetMotor RWheelEnable LWheelEnable')
+
+	try:
+
+		#Acercarse al muro
+		while getLaserValues(0) > 60:
+			envia(ser, 'SetMotor LWheelDist 100 RWheelDist 100 Speed 50')
+
+		#Girar para tener el muro a la derecha
+		while getLaserValues(8) > 40:
+			envia(ser, 'SetMotor LWheelDist 0 RWheelDist 180 Speed 100')
+		while True:
+			#cerca del muro
+			if getLaserValues(8) < 30:
+				envia(ser, 'SetMotor LWheelDist 0 RWheelDist 100 Speed 100')
+				time.sleep(0.1)
+	        #lejos el muro
+			elif getLaserValues(8) > 30:
+				envia(ser, 'SetMotor LWheelDist 100 RWheelDist 0 Speed 100')
+				time.sleep(0.1)
+			envia(ser, 'SetMotor LWheelDist 100 RWheelDist 100 Speed 50')
+		
+
+
+		envia(ser, 'TestMode Off', 0.2)
+
+		# Close the Serial Port.
+		ser.close()
+		print "Final"
+	except KeyboardInterrupt:
+		print "Final"
