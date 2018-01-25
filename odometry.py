9be49b83f4029524bfe73749317306c351b87c71
DavidLozano13 <davidlozano1311@gmail.com>
2018-01-25 13:03:51 +0100

    odometry ok

--- odometry.py
+++ odometry.py
@@ -6,6 +6,7 @@ import time
 from multiprocessing import Process, Queue
 import http_viewer
 import math
+import numpy
 import random
 
 ###################### !!! W A R N I N G !!! ########################
@@ -26,24 +27,6 @@ def get(message):
     #print "GET - Message: %s" %message;
     ser.write(message+'\r'+'\n');
 
-def getLDS():
-    set('SetLDSRotation On', 1500);
-    get('GetLDSScan');
-    
-    LDS_data = [];
-    while ser.inWaiting()>0:
-        line = ser.readline();
-        line_split = line.split(",");
-        if (len(line_split) == 4 and line_split[0].isdigit()):
-            #Array ['AngleInDegrees', 'DistInMM', 'errorCode']
-            line_content = [line_split[0], line_split[1], line_split[3]];
-            LDS_data.append(line_content);
-    
-    set('SetLDSRotation Off', 100);
-   
-    return LDS_data;
-
-
 def get_motors():
 	""" Ask to the robot for the current state of the motors. """
 	msg = envia(ser, 'GetMotors LeftWheel RightWheel').split('\n')
@@ -61,21 +44,60 @@ def odometry(L, R):
 	global suma_theta
 	global L_ini, R_ini
 	#Implement the pos integration. Assume initial conditions [0,0,0].
-	delta_d = (R+L)/2
-	delta_th = (R-L)/243
+	
 	#Use global variables, discoment this line
 	#global VARIABLES
 	new_L, new_R = L - L_ini, R - R_ini
 
 	L_ini = L
 	R_ini = R
+
+	delta_d = (new_R+new_L)/2
+	delta_th = (new_R-new_L)/243
 	suma_theta = (suma_theta+delta_th)
+	suma_theta = numpy.mod(suma_theta,2*math.pi)
 	dx = delta_d*math.cos(suma_theta)
 	dy = delta_d*math.sin(suma_theta)
-	print (dx,dy,suma_theta)
 	x = x + dx
 	y = y + dy
-	# Return x_word, y_word, theta_word
+	return [x,y,suma_theta]
+
+def driveTo(X, Y):
+	L, R = get_motors()
+	Odo = odometry(L,R)
+	time.sleep(0.1)
+	DistX = X - Odo[0]
+	DistY = Y - Odo[1]
+	difAngle = numpy.arctan2(DistY,DistX)
+	if Odo[2] - difAngle > 2*math.pi - (Odo[2] - difAngle):
+		NewTheta = -(2*math.pi - (Odo[2] - difAngle))
+	else: 
+		NewTheta = Odo[2] - difAngle
+
+	envia(ser, 'SetMotor LWheelDist '+ str(NewTheta*121.5) +' RWheelDist ' + str(-NewTheta*121.5) + ' Speed ' + str(100))
+	L, R = get_motors()
+	PostOdo = odometry(L, R)
+	time.sleep(0.1)
+	PrevOdo = PostOdo[:]
+	PrevOdo[0] = PrevOdo[0] + 1
+	while PrevOdo != PostOdo :
+		L, R = get_motors()
+		PrevOdo = PostOdo[:]
+		PostOdo = odometry(L, R)
+		time.sleep(0.1)
+
+	Dist = numpy.sqrt(DistX*DistX + DistY*DistY)
+	envia(ser, 'SetMotor LWheelDist '+ str(Dist) +' RwheelDist ' + str(Dist) + ' Speed ' + str(200))
+        L, R = get_motors()
+        PostOdo = odometry(L, R)
+        time.sleep(0.1)
+        PrevOdo = PostOdo[:]
+        PrevOdo[0] = PrevOdo[0] + 1
+        while PrevOdo != PostOdo :
+            L, R = get_motors()
+            PrevOdo = PostOdo[:]
+            PostOdo = odometry(L, R)
+            time.sleep(0.1)
 
 if __name__ == "__main__":
 
@@ -101,21 +123,12 @@ if __name__ == "__main__":
 	suma_theta = 0
 	x = y = 0
 
-	#envia(ser, 'SetMotor LWheelDist '+ str(100) +' RWheelDist ' + str(100) + ' Speed ' + str(speed))
-
 	try:
 		i = 0
 		L, R = get_motors()
-		while True:
-			L, R = get_motors()
-			r_queue.put([(-L/40.0, i), (100,100)])
-			l_queue.put([(-R/40.0, i), (200,100)])
-
-			odometry(L, R)
-			time.sleep(0.1)
-			i+=60
-
-
+		r_queue.put([(-L/40.0, i), (100,100)])
+		l_queue.put([(-R/40.0, i), (200,100)])
+		driveTo(500, 500)
 		envia(ser, 'TestMode Off', 0.2)
 
 		# Close the Serial Port.
