60feb129b4bad4cb62812cbe2a2c84c121311949
xavip <xavi@client91-10-193-23.xsfupc.upc.edu>
2018-01-25 16:53:17 +0100

    fixing odometry and adding follow enemy

---
+++ follow_enemy.py
@@ -0,0 +1,148 @@
+# To import the function "envia" from the file "test_NeatoCommands.py"
+from test_NeatoCommands import envia
+import sys
+import serial
+import time
+from multiprocessing import Process, Queue
+import math
+import random
+
+def set(message, timeInMs):
+	#print "SET - Message: %s, SleepTimeInMs: %f" %(message, timeInMs);
+	ser.write(message+'\r'+'\n');
+	time.sleep(abs(timeInMs/1000));
+		
+def get(message):
+	#print "GET - Message: %s" %message;
+	ser.write(message+'\r'+'\n');
+
+def getLDS():
+	msg = envia(ser, 'GetLDSScan',0.2,False)
+	var = []
+	for line in msg.split('\r\n')[2:362]:
+		s = line.split(',')
+		var.append([s[0], s[1], s[2], s[3]])
+	return var
+
+
+def get_motors():
+	""" Ask to the robot for the current state of the motors. """
+	msg = envia(ser, 'GetMotors LeftWheel RightWheel').split('\n')
+
+	# For better understanding see the neato commands PDF.
+
+	L = int(msg[4].split(',')[1])
+	R = int(msg[8].split(',')[1])
+
+	return (L, R)
+
+def getLaserValues():
+	global speed
+	res = [2000,2000,2000,2000,2000,2000,2000,2000,2000,2000]
+	msg = getLDS()
+	values = [2000 for num in range(360)]
+	for value in msg:
+		if int(value[1]) > 0:
+			values[int(value[0])] = int(value[1])
+	# Coger 5 posiciones: 0-36, 37-72, 73-108, 109-144, 145-180
+	# Init
+	res[0] = values[0]
+	res[1] = values[36]
+	res[2] = values[72]
+	res[3] = values[108]
+	res[4] = values[144]
+	res[5] = values[180]
+	res[6] = values[216]
+	res[7] = values[252]
+	res[8] = values[288]
+	res[9] = values[324]
+
+	for i in range(0,359):
+		if i >= 0 and i <= 35:
+			# Zona izquierda
+			if values[i] < res[0] and values[i] != 0:
+				res[0] = values[i]
+		if i >= 36 and i <= 71:
+			# Zona izquierda central
+			if values[i] < res[1] and values[i] != 0:
+				res[1] = values[i]
+		if i >= 72 and i <= 107:
+			# Zona central
+			if values[i] < res[2] and values[i] != 0:
+				res[2] = values[i]
+		if i >= 108 and i <= 143:
+			# Zona derecha central
+			if values[i] < res[3] and values[i] != 0:
+				res[3] = values[i]
+		if i >= 144 and i <= 179:
+			# Zona derecha
+			if values[i] < res[4] and values[i] != 0:
+				res[4] = values[i]
+		if i >= 180 and i <= 215:
+			# Zona izquierda
+			if values[i] < res[5] and values[i] != 0:
+				res[5] = values[i]
+		if i >= 216 and i <= 251:
+			# Zona izquierda central
+			if values[i] < res[6] and values[i] != 0:
+				res[6] = values[i]
+		if i >= 252 and i <= 287:
+			# Zona central
+			if values[i] < res[7] and values[i] != 0:
+				res[7] = values[i]
+		if i >= 288 and i <= 323:
+			# Zona derecha central
+			if values[i] < res[8] and values[i] != 0:
+				res[8] = values[i]
+		if i >= 324 and i <= 359:
+			# Zona derecha
+			if values[i] < res[9] and values[i] != 0:
+				res[9] = values[i]
+
+	resfinal = [res[2], res[1], res[0], res[9], res[8]]
+
+	res[0]=res[0]/10
+	res[1]=res[1]/10
+	res[2]=res[2]/10
+	res[8]=res[8]/10
+	res[9]=res[9]/10
+
+
+	speed = 300
+	distR = ((res[8]+res[9])/2) + res[0]
+	distL = ((res[2]+res[1])/2) + res[0]
+
+	distL = distL * speed / 100
+	distR = distR * speed / 100
+	print('SetMotor LWheelDist '+ str(distL) +' RWheelDist ' + str(distR) + ' Speed ' + str(speed))
+	envia(ser, 'SetMotor LWheelDist '+ str(distL) +' RWheelDist ' + str(distR) + ' Speed ' + str(speed))
+	return res
+
+if __name__ == "__main__":
+
+	# Open the Serial Port.
+	global ser
+	ser = serial.Serial(port='/dev/ttyACM0', baudrate=115200, timeout=0.05)
+	envia(ser, 'TestMode On')
+
+	envia(ser, 'PlaySound 1')
+
+	envia(ser ,'SetMotor RWheelEnable LWheelEnable')
+	envia(ser, 'SetLDSRotation On',0.2,False)
+
+	#envia(ser, 'SetMotor LWheelDist '+ str(100) +' RWheelDist ' + str(100) + ' Speed ' + str(speed))
+
+	try:
+		L, R = get_motors()
+		while True:
+			getLaserValues()
+			time.sleep(0.1)
+
+		envia(ser, 'TestMode Off', 0.2)
+
+		# Close the Serial Port.
+		ser.close()
+		print "Final"
+	except KeyboardInterrupt:
+		envia(ser, 'SetLDSRotation Off',0.2,False)
+		print "keyboardInterrupt"
--- odometry.py
+++ odometry.py
@@ -4,14 +4,15 @@ import sys
 import serial
 import time
 from multiprocessing import Process, Queue
-import http_viewer
 import math
 import numpy
-import random
 
 x=0
 y=0
 suma_theta=0
+distance_weels_mid = 121.5
+distance_robot = 318
+speed = 125
 
 
 def set(message, timeInMs):
@@ -39,10 +40,7 @@ def odometry(L, R):
 	global y
 	global suma_theta
 	global L_ini, R_ini
-	#Implement the pos integration. Assume initial conditions [0,0,0].
-	
-	#Use global variables, discoment this line
-	#global VARIABLES
+
 	new_L, new_R = L - L_ini, R - R_ini
 
 	L_ini = L
@@ -59,45 +57,26 @@ def odometry(L, R):
 	return [x,y,suma_theta]
 
 def driveTo(X, Y):
-	print('====================')
 	L, R = get_motors()
-	Odo = odometry(L,R)
-	time.sleep(0.1)
-	print('inside 1 '+str(Odo[2]))
-	DistX = X - Odo[0]
-	DistY = Y - Odo[1]
-	difAngle = numpy.arctan2(DistY,DistX)
-	if Odo[2] - difAngle > 2*math.pi - (Odo[2] - difAngle):
-		NewTheta = -(2*math.pi - (Odo[2] - difAngle))
-		print('inside 1 '+str(NewTheta))
+	odom = odometry(L,R)
+	x_go = X - odom[0]
+	y_go = Y - odom[1]
+
+	angleRest = numpy.arctan2(y_go,x_go)
+	if odom[2] - angleRest > 2*math.pi - (odom[2] - angleRest):
+		angle = - (2 * math.pi - (odom[2] - angleRest))
 	else: 
-		NewTheta = Odo[2] - difAngle
-		print('inside 2 '+str(NewTheta))
+		angle = odom[2] - angleRest
 
-	envia(ser, 'SetMotor LWheelDist '+ str(NewTheta*121.5) +' RWheelDist ' + str(-NewTheta*121.5) + ' Speed 120')
-	L, R = get_motors()
-	PostOdo = odometry(L, R)
-	time.sleep(0.1)
-	PrevOdo = PostOdo[:]
-	PrevOdo[0] = PrevOdo[0] + 1
-	while PrevOdo != PostOdo :
-		L, R = get_motors()
-		PrevOdo = PostOdo[:]
-		PostOdo = odometry(L, R)
-		time.sleep(0.1)
+	envia(ser, 'SetMotor LWheelDist '+ str(angle*distance_weels_mid) +' RWheelDist ' + str(-angle*distance_weels_mid) + ' Speed '+str(speed))
+	time.sleep(abs((angle*distance_weels_mid)/speed))
+	
+	Dist = numpy.sqrt(x_go*x_go + y_go*y_go)
+	envia(ser, 'SetMotor LWheelDist '+ str(Dist) +' RwheelDist ' + str(Dist) + ' Speed '+str(speed))
+	time.sleep(Dist/speed)
 
-	Dist = numpy.sqrt(DistX*DistX + DistY*DistY)
-	envia(ser, 'SetMotor LWheelDist '+ str(Dist) +' RwheelDist ' + str(Dist) + ' Speed 120')
-        L, R = get_motors()
-        PostOdo = odometry(L, R)
-        time.sleep(0.1)
-        PrevOdo = PostOdo[:]
-        PrevOdo[0] = PrevOdo[0] + 1
-        while PrevOdo != PostOdo :
-            L, R = get_motors()
-            PrevOdo = PostOdo[:]
-            PostOdo = odometry(L, R)
-            time.sleep(0.1)
+	envia(ser, 'SetMotor LWheelDist '+ str(-angle*distance_weels_mid) +' RWheelDist ' + str(angle*distance_weels_mid) + ' Speed '+str(speed))
+	time.sleep(abs((angle*distance_weels_mid)/speed))
 
 if __name__ == "__main__":
 	# Open the Serial Port.
@@ -118,7 +97,8 @@ if __name__ == "__main__":
 	try:
 		i = 0
 		L_ini, R_ini = get_motors()
-		driveTo(-500, 0)
+		
+		driveTo(800, 800)
 		envia(ser, 'TestMode Off', 0.2)
 
 		# Close the Serial Port.
